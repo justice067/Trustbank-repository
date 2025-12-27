1659f34aaa23d8134923ff84fb3ce44325552d69
justice067 <christopherscott5658@gmail.com>
2025-12-26 19:22:01 -0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,282 +1,246 @@
-import pkgutil
-from importlib import import_module
+import json
+from collections import UserList
 
 from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
-
-# For backwards compatibility with Django < 3.2
-from django.utils.connection import ConnectionDoesNotExist  # NOQA: F401
-from django.utils.connection import BaseConnectionHandler
-from django.utils.functional import cached_property
-from django.utils.module_loading import import_string
-
-DEFAULT_DB_ALIAS = "default"
-DJANGO_VERSION_PICKLE_KEY = "_django_version"
+from django.core.exceptions import ValidationError
+from django.forms.renderers import get_default_renderer
+from django.utils import timezone
+from django.utils.html import escape, format_html_join
+from django.utils.safestring import mark_safe
+from django.utils.translation import gettext_lazy as _
 
 
-class Error(Exception):
-    pass
+def pretty_name(name):
+    """Convert 'first_name' to 'First name'."""
+    if not name:
+        return ""
+    return name.replace("_", " ").capitalize()
 
 
-class InterfaceError(Error):
-    pass
-
-
-class DatabaseError(Error):
-    pass
-
-
-class DataError(DatabaseError):
-    pass
-
-
-class OperationalError(DatabaseError):
-    pass
-
-
-class IntegrityError(DatabaseError):
-    pass
-
-
-class InternalError(DatabaseError):
-    pass
-
-
-class ProgrammingError(DatabaseError):
-    pass
-
-
-class NotSupportedError(DatabaseError):
-    pass
-
-
-class DatabaseErrorWrapper:
+def flatatt(attrs):
     """
-    Context manager and decorator that reraises backend-specific database
-    exceptions using Django's common wrappers.
+    Convert a dictionary of attributes to a single string.
+    The returned string will contain a leading space followed by key="value",
+    XML-style pairs. In the case of a boolean value, the key will appear
+    without a value. It is assumed that the keys do not need to be
+    XML-escaped. If the passed dictionary is empty, then return an empty
+    string.
+
+    The result is passed through 'mark_safe' (by way of 'format_html_join').
     """
+    key_value_attrs = []
+    boolean_attrs = []
+    for attr, value in attrs.items():
+        if isinstance(value, bool):
+            if value:
+                boolean_attrs.append((attr,))
+        elif value is not None:
+            key_value_attrs.append((attr, value))
 
-    def __init__(self, wrapper):
-        """
-        wrapper is a database wrapper.
-
-        It must have a Database attribute defining PEP-249 exceptions.
-        """
-        self.wrapper = wrapper
-
-    def __del__(self):
-        del self.wrapper
-
-    def __enter__(self):
-        pass
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        if exc_type is None:
-            return
-        for dj_exc_type in (
-            DataError,
-            OperationalError,
-            IntegrityError,
-            InternalError,
-            ProgrammingError,
-            NotSupportedError,
-            DatabaseError,
-            InterfaceError,
-            Error,
-        ):
-            db_exc_type = getattr(self.wrapper.Database, dj_exc_type.__name__)
-            if issubclass(exc_type, db_exc_type):
-                dj_exc_value = dj_exc_type(*exc_value.args)
-                # Only set the 'errors_occurred' flag for errors that may make
-                # the connection unusable.
-                if dj_exc_type not in (DataError, IntegrityError):
-                    self.wrapper.errors_occurred = True
-                raise dj_exc_value.with_traceback(traceback) from exc_value
-
-    def __call__(self, func):
-        # Note that we are intentionally not using @wraps here for performance
-        # reasons. Refs #21109.
-        def inner(*args, **kwargs):
-            with self:
-                return func(*args, **kwargs)
-
-        return inner
+    return format_html_join("", ' {}="{}"', sorted(key_value_attrs)) + format_html_join(
+        "", " {}", sorted(boolean_attrs)
+    )
 
 
-def load_backend(backend_name):
-    """
-    Return a database backend's "base" module given a fully qualified database
-    backend name, or raise an error if it doesn't exist.
-    """
-    # This backend was renamed in Django 1.9.
-    if backend_name == "django.db.backends.postgresql_psycopg2":
-        backend_name = "django.db.backends.postgresql"
-
-    try:
-        return import_module("%s.base" % backend_name)
-    except ImportError as e_user:
-        # The database backend wasn't found. Display a helpful error message
-        # listing all built-in database backends.
-        import django.db.backends
-
-        builtin_backends = [
-            name
-            for _, name, ispkg in pkgutil.iter_modules(django.db.backends.__path__)
-            if ispkg and name not in {"base", "dummy"}
-        ]
-        if backend_name not in ["django.db.backends.%s" % b for b in builtin_backends]:
-            backend_reprs = map(repr, sorted(builtin_backends))
-            raise ImproperlyConfigured(
-                "%r isn't an available database backend or couldn't be "
-                "imported. Check the above exception. To use one of the "
-                "built-in backends, use 'django.db.backends.XXX', where XXX "
-                "is one of:\n"
-                "    %s" % (backend_name, ", ".join(backend_reprs))
-            ) from e_user
-        else:
-            # If there's some other error, this must be an error in Django
-            raise
-
-
-class ConnectionHandler(BaseConnectionHandler):
-    settings_name = "DATABASES"
-    # Connections needs to still be an actual thread local, as it's truly
-    # thread-critical. Database backends should use @async_unsafe to protect
-    # their code from async contexts, but this will give those contexts
-    # separate connections in case it's needed as well. There's no cleanup
-    # after async contexts, though, so we don't allow that if we can help it.
-    thread_critical = True
-
-    def configure_settings(self, databases):
-        databases = super().configure_settings(databases)
-        if databases == {}:
-            databases[DEFAULT_DB_ALIAS] = {"ENGINE": "django.db.backends.dummy"}
-        elif DEFAULT_DB_ALIAS not in databases:
-            raise ImproperlyConfigured(
-                f"You must define a '{DEFAULT_DB_ALIAS}' database."
-            )
-        elif databases[DEFAULT_DB_ALIAS] == {}:
-            databases[DEFAULT_DB_ALIAS]["ENGINE"] = "django.db.backends.dummy"
-
-        # Configure default settings.
-        for conn in databases.values():
-            conn.setdefault("ATOMIC_REQUESTS", False)
-            conn.setdefault("AUTOCOMMIT", True)
-            conn.setdefault("ENGINE", "django.db.backends.dummy")
-            if conn["ENGINE"] == "django.db.backends." or not conn["ENGINE"]:
-                conn["ENGINE"] = "django.db.backends.dummy"
-            conn.setdefault("CONN_MAX_AGE", 0)
-            conn.setdefault("CONN_HEALTH_CHECKS", False)
-            conn.setdefault("OPTIONS", {})
-            conn.setdefault("TIME_ZONE", None)
-            for setting in ["NAME", "USER", "PASSWORD", "HOST", "PORT"]:
-                conn.setdefault(setting, "")
-
-            test_settings = conn.setdefault("TEST", {})
-            default_test_settings = [
-                ("CHARSET", None),
-                ("COLLATION", None),
-                ("MIGRATE", True),
-                ("MIRROR", None),
-                ("NAME", None),
-            ]
-            for key, value in default_test_settings:
-                test_settings.setdefault(key, value)
-        return databases
-
-    @property
-    def databases(self):
-        # Maintained for backward compatibility as some 3rd party packages have
-        # made use of this private API in the past. It is no longer used within
-        # Django itself.
-        return self.settings
-
-    def create_connection(self, alias):
-        db = self.settings[alias]
-        backend = load_backend(db["ENGINE"])
-        return backend.DatabaseWrapper(db, alias)
-
-
-class ConnectionRouter:
-    def __init__(self, routers=None):
-        """
-        If routers is not specified, default to settings.DATABASE_ROUTERS.
-        """
-        self._routers = routers
-
-    @cached_property
-    def routers(self):
-        if self._routers is None:
-            self._routers = settings.DATABASE_ROUTERS
-        routers = []
-        for r in self._routers:
-            if isinstance(r, str):
-                router = import_string(r)()
-            else:
-                router = r
-            routers.append(router)
-        return routers
-
-    def _router_func(action):
-        def _route_db(self, model, **hints):
-            chosen_db = None
-            for router in self.routers:
-                try:
-                    method = getattr(router, action)
-                except AttributeError:
-                    # If the router doesn't have a method, skip to the next
-                    # one.
-                    pass
-                else:
-                    chosen_db = method(model, **hints)
-                    if chosen_db:
-                        return chosen_db
-            instance = hints.get("instance")
-            if instance is not None and instance._state.db:
-                return instance._state.db
-            return DEFAULT_DB_ALIAS
-
-        return _route_db
-
-    db_for_read = _router_func("db_for_read")
-    db_for_write = _router_func("db_for_write")
-
-    def allow_relation(self, obj1, obj2, **hints):
-        for router in self.routers:
-            try:
-                method = router.allow_relation
-            except AttributeError:
-                # If the router doesn't have a method, skip to the next one.
-                pass
-            else:
-                allow = method(obj1, obj2, **hints)
-                if allow is not None:
-                    return allow
-        return obj1._state.db == obj2._state.db
-
-    def allow_migrate(self, db, app_label, **hints):
-        for router in self.routers:
-            try:
-                method = router.allow_migrate
-            except AttributeError:
-                # If the router doesn't have a method, skip to the next one.
-                continue
-
-            allow = method(db, app_label, **hints)
-
-            if allow is not None:
-                return allow
-        return True
-
-    def allow_migrate_model(self, db, model):
-        return self.allow_migrate(
-            db,
-            model._meta.app_label,
-            model_name=model._meta.model_name,
-            model=model,
+class RenderableMixin:
+    def get_context(self):
+        raise NotImplementedError(
+            "Subclasses of RenderableMixin must provide a get_context() method."
         )
 
-    def get_migratable_models(self, app_config, db, include_auto_created=False):
-        """Return app models allowed to be migrated on provided db."""
-        models = app_config.get_models(include_auto_created=include_auto_created)
-        return [model for model in models if self.allow_migrate_model(db, model)]
+    def render(self, template_name=None, context=None, renderer=None):
+        renderer = renderer or self.renderer
+        template = template_name or self.template_name
+        context = context or self.get_context()
+        return mark_safe(renderer.render(template, context))
+
+    __str__ = render
+    __html__ = render
+
+
+class RenderableFieldMixin(RenderableMixin):
+    def as_field_group(self):
+        return self.render()
+
+    def as_hidden(self):
+        raise NotImplementedError(
+            "Subclasses of RenderableFieldMixin must provide an as_hidden() method."
+        )
+
+    def as_widget(self):
+        raise NotImplementedError(
+            "Subclasses of RenderableFieldMixin must provide an as_widget() method."
+        )
+
+    def __str__(self):
+        """Render this field as an HTML widget."""
+        if self.field.show_hidden_initial:
+            return self.as_widget() + self.as_hidden(only_initial=True)
+        return self.as_widget()
+
+    __html__ = __str__
+
+
+class RenderableFormMixin(RenderableMixin):
+    def as_p(self):
+        """Render as <p> elements."""
+        return self.render(self.template_name_p)
+
+    def as_table(self):
+        """Render as <tr> elements excluding the surrounding <table> tag."""
+        return self.render(self.template_name_table)
+
+    def as_ul(self):
+        """Render as <li> elements excluding the surrounding <ul> tag."""
+        return self.render(self.template_name_ul)
+
+    def as_div(self):
+        """Render as <div> elements."""
+        return self.render(self.template_name_div)
+
+
+class RenderableErrorMixin(RenderableMixin):
+    def as_json(self, escape_html=False):
+        return json.dumps(self.get_json_data(escape_html))
+
+    def as_text(self):
+        return self.render(self.template_name_text)
+
+    def as_ul(self):
+        return self.render(self.template_name_ul)
+
+
+class ErrorDict(dict, RenderableErrorMixin):
+    """
+    A collection of errors that knows how to display itself in various formats.
+
+    The dictionary keys are the field names, and the values are the errors.
+    """
+
+    template_name = "django/forms/errors/dict/default.html"
+    template_name_text = "django/forms/errors/dict/text.txt"
+    template_name_ul = "django/forms/errors/dict/ul.html"
+
+    def __init__(self, *args, renderer=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.renderer = renderer or get_default_renderer()
+
+    def as_data(self):
+        return {f: e.as_data() for f, e in self.items()}
+
+    def get_json_data(self, escape_html=False):
+        return {f: e.get_json_data(escape_html) for f, e in self.items()}
+
+    def get_context(self):
+        return {
+            "errors": self.items(),
+            "error_class": "errorlist",
+        }
+
+
+class ErrorList(UserList, list, RenderableErrorMixin):
+    """
+    A collection of errors that knows how to display itself in various formats.
+    """
+
+    template_name = "django/forms/errors/list/default.html"
+    template_name_text = "django/forms/errors/list/text.txt"
+    template_name_ul = "django/forms/errors/list/ul.html"
+
+    def __init__(self, initlist=None, error_class=None, renderer=None, field_id=None):
+        super().__init__(initlist)
+
+        if error_class is None:
+            self.error_class = "errorlist"
+        else:
+            self.error_class = "errorlist {}".format(error_class)
+        self.renderer = renderer or get_default_renderer()
+        self.field_id = field_id
+
+    def as_data(self):
+        return ValidationError(self.data).error_list
+
+    def copy(self):
+        copy = super().copy()
+        copy.error_class = self.error_class
+        copy.renderer = self.renderer
+        return copy
+
+    def get_json_data(self, escape_html=False):
+        errors = []
+        for error in self.as_data():
+            message = next(iter(error))
+            errors.append(
+                {
+                    "message": escape(message) if escape_html else message,
+                    "code": error.code or "",
+                }
+            )
+        return errors
+
+    def get_context(self):
+        return {
+            "errors": self,
+            "error_class": self.error_class,
+        }
+
+    def __repr__(self):
+        return repr(list(self))
+
+    def __contains__(self, item):
+        return item in list(self)
+
+    def __eq__(self, other):
+        return list(self) == other
+
+    def __getitem__(self, i):
+        error = self.data[i]
+        if isinstance(error, ValidationError):
+            return next(iter(error))
+        return error
+
+    def __reduce_ex__(self, *args, **kwargs):
+        # The `list` reduce function returns an iterator as the fourth element
+        # that is normally used for repopulating. Since we only inherit from
+        # `list` for `isinstance` backward compatibility (Refs #17413) we
+        # nullify this iterator as it would otherwise result in duplicate
+        # entries. (Refs #23594)
+        info = super(UserList, self).__reduce_ex__(*args, **kwargs)
+        return info[:3] + (None, None)
+
+
+# Utilities for time zone support in DateTimeField et al.
+
+
+def from_current_timezone(value):
+    """
+    When time zone support is enabled, convert naive datetimes
+    entered in the current time zone to aware datetimes.
+    """
+    if settings.USE_TZ and value is not None and timezone.is_naive(value):
+        current_timezone = timezone.get_current_timezone()
+        try:
+            if timezone._datetime_ambiguous_or_imaginary(value, current_timezone):
+                raise ValueError("Ambiguous or non-existent time.")
+            return timezone.make_aware(value, current_timezone)
+        except Exception as exc:
+            raise ValidationError(
+                _(
+                    "%(datetime)s couldn’t be interpreted "
+                    "in time zone %(current_timezone)s; it "
+                    "may be ambiguous or it may not exist."
+                ),
+                code="ambiguous_timezone",
+                params={"datetime": value, "current_timezone": current_timezone},
+            ) from exc
+    return value
+
+
+def to_current_timezone(value):
+    """
+    When time zone support is enabled, convert aware datetimes
+    to naive datetimes in the current time zone for display.
+    """
+    if settings.USE_TZ and value is not None and timezone.is_aware(value):
+        return timezone.make_naive(value)
+    return value
